b7426b8caa3715ebb38b3a70b17426a9a1bc251a
xychen6871 <82066951+xychen6871@users.noreply.github.com>
2021-09-14 21:42:28 -0700

    Add files via upload
    Work in progress.

---
+++ Model.h
@@ -0,0 +1,30 @@
+#include <vector>
+#include <string>
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+#include "Mesh.h"
+#include "Texture.h"
+#pragma once
+class Model
+{
+private:
+	std::vector<Mesh*> meshList;
+	std::vector<Texture*> textureList;
+	std::vector<unsigned int> meshToTex;
+
+	void LoadNode(aiNode* node, const aiScene* scene);
+	void LoadMesh(aiMesh* mesh, const aiScene* scene);
+	void LoadMaterials(const aiScene* scene);
+
+public:
+	Model();
+
+	void LoadModel(const std::string& fileName);
+	void RenderModel();
+	void ClearModel();
+
+	~Model();
+};
